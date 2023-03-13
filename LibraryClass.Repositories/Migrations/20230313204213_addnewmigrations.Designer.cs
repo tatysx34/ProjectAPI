c0dde3f22c86da217fa39dbce8b456776f21fe7f
Tatiana Ramirez madrigal <tatiana@Tatianas-MacBook-Air.local>
2023-03-13 14:43:04 -0600

    new migrations

--- LibraryClass.Repositories/Migrations/20230313204213_addnewmigrations.Designer.cs
+++ LibraryClass.Repositories/Migrations/20230313204213_addnewmigrations.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LibraryClass.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LibraryClass.Repositories.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230313204213_addnewmigrations")]
+    partial class addnewmigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LibraryClass.Repositories/Migrations/20230313204213_addnewmigrations.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryClass.Repositories.Migrations
+{
+    public partial class addnewmigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Province",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StreetAddress",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Province",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "StreetAddress",
+                table: "Users");
+        }
+    }
+}
